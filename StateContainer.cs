c695b4c98059cd993ddd8f46e551c6470271de5e
cheng_philll@knights.ucf.edu <cheng_philll@knights.ucf.edu>
2021-01-14 19:57:27 -0500

    First commit - tested code and learned a bit about Blazor

---
+++ StateContainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace AIOPurchaseBot
+{
+    public class StateContainer
+    {
+        public int CurrentCountState { get; set; } = 0;
+        public event Action OnCountChange;
+        public void SetProperty(int value) 
+        {
+            CurrentCountState = value;
+            NotifyStateChange();
+        }
+
+        private void NotifyStateChange() => OnCountChange?.Invoke();
+    }
+}
